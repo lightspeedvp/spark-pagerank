3f1c9365d181fe703c4b0ed80fbd8756ef1c3fbf
Eric Wayman <eric@ERWA-MBP15.local>
2019-09-26 09:41:12 -0700

    WIP.  Added test to GraphBuilderAppTest.scala to test running from graph

--- src/test/scala/com.soundcloud.spark.pagerank/GraphBuilderAppTest.scala
+++ src/test/scala/com.soundcloud.spark.pagerank/GraphBuilderAppTest.scala
@@ -3,7 +3,8 @@ package com.soundcloud.spark.pagerank
 import java.io.File
 
 import org.apache.commons.io.FileUtils
-import org.scalatest.{ BeforeAndAfter, FunSuite, Matchers }
+import org.scalatest.{BeforeAndAfter, FunSuite, Matchers}
+import org.apache.spark.storage.StorageLevel
 
 class GraphBuilderAppTest
   extends FunSuite
@@ -13,6 +14,7 @@ class GraphBuilderAppTest
   with SparkTesting {
 
   val path = "target/test/GraphBuilderAppTest"
+  val graphPath = "/Users/eric/Desktop/GraphBuilderAppTest"
 
   before {
     FileUtils.deleteDirectory(new File(path))
@@ -35,5 +37,42 @@ class GraphBuilderAppTest
     ).map(_.productIterator.toSeq.mkString("\t")))
 
     GraphBuilderApp.runFromInputs(options, spark, input)
+  }
+
+  test("test build graph from file"){
+      val options = new GraphBuilderApp.Options()
+      options.output = graphPath
+      // "Input directory containing edges in TSV format (source, destination, weight)"
+      options.input = "/Users/eric/Desktop/edgeGraph.tsv"
+      options.numPartitions = 1
+
+      GraphBuilderApp.runFromInputs(
+        options, spark, spark.sparkContext.textFile(options.input, minPartitions = options.numPartitions)
+      )
+  }
+
+  test("run pageRank from graph file"){
+    val options = new PageRankApp.Options()
+    options.input = graphPath
+    options.output = "/Users/eric/Desktop/pageRankAppRankingsTest/"
+    val graph = PageRankGraph.load(
+      spark,
+      options.input,
+      edgesStorageLevel = StorageLevel.MEMORY_AND_DISK_2,
+      verticesStorageLevel = StorageLevel.MEMORY_AND_DISK_2
+    )
+
+    PageRankApp.runFromInputs(
+      spark,
+      options,
+      graph,
+      priorsOpt = None
+    )
+//    spark.sparkContext.setCheckpointDir(spark.conf.get("spark.local.dir"))
+//    val rankings = PageRank.run(graph = graph)
+//    rankings.map{case(a, b) =>
+//      var line = a.toString + "," + b.toString
+//      line
+//    }.saveAsTextFile(options.output + "rankings.csv");
   }
 }
